b3d2e15184d16921bd12161f6a7c24bbf7428c2b
Charing Chen <charinghaxen@gmail.com>
2020-08-07 17:32:53 -0700

    Created model and saved couple

---
+++ model.py
@@ -0,0 +1,64 @@
+import csv
+import cv2
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers.convolutional import Conv2D
+
+lines = []
+
+with open('./sample_data/data/data/driving_log.csv')as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []
+break_line = '\\'  # for windows \\, linux use /
+
+for line in lines:
+    source_path = line[0]
+    filename = source_path.split(break_line)[-1]
+    current_path = './sample_data/data/data/' + filename
+    image = cv2.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+
+# Image processing
+augmented_images, augmented_measurements = [], []
+# image flipping
+for image, measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image, 1))
+    augmented_measurements.append(measurement * -1)
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
+
+print(X_train.shape)
+
+model = Sequential()
+# Cropping the image
+model.add(Cropping2D(cropping=((70, 20), (0, 0)), input_shape=(160, 320, 3)))
+# Normaliziation
+model.add(Lambda(lambda x: x / 255. - 0.5))
+
+# Use of ElU: http://image-net.org/challenges/posters/JKU_EN_RGB_Schwarz_poster.pdf
+
+model.add(Conv2D(24, 5, strides=(2, 2), activation='elu'))
+model.add(Conv2D(36, 5, strides=(2, 2), activation='elu'))
+model.add(Conv2D(48, 5, strides=(2,2), activation='elu'))
+model.add(Conv2D(64, 3, activation='elu'))
+model.add(Conv2D(64, 3, activation='elu'))
+model.add(Flatten())
+# model.add(Dense(1152,activation='elu'))
+model.add(Dense(100, activation='elu'))
+model.add(Dense(50, activation='elu'))
+model.add(Dense(10, activation='elu'))
+model.add(Dense(1, activation='elu'))
+model.summary()
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=4)
+model.save('model_sample.h5')
